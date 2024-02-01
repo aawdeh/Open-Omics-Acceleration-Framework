479c98a41eb348d17ee7171acb449efb30fc76bf
aawdeh <awdeh@broadinstitute.org>
2024-02-01 17:31:51 -0500

    Convert bwa to STAR

---
+++ pipelines/optimus_starsolo/run_starsolo.sh
@@ -0,0 +1,136 @@
+source config
+
+mode="multifq2sortedbam"
+if [ "$#" == "1" ]
+    then
+    mode=$1
+else
+    echo "<exec> <pragzip/flatmode/fqprocessonly/multifq2sortedbam>"
+    exit
+fi
+echo "mode: "$mode
+source miniconda3/bin/activate distbwa
+
+echo "localhost" > hostfile
+num_nodes=1
+lscpu > compute_config
+
+
+num_cpus_per_node=$(cat compute_config | grep -E '^CPU\(s\)' | awk  '{print $2}')
+num_socket=$(cat compute_config | grep -E '^Socket'| awk  '{print $2}')
+num_numa=$(cat compute_config | grep '^NUMA node(s)' | awk '{print $3}')
+num_cpus_all_node=`expr ${num_cpus_per_node} \* ${num_nodes}`
+threads_per_core=$(cat compute_config | grep -E '^Thread' | awk  '{print $4}')
+echo "#############################################"
+echo "Total number of CPUs: $num_cpus_all_node"
+echo "Number of sockets: "$num_socket
+echo "Number of NUMA domains: "$num_numa
+
+num_physical_cores_all_nodes=`expr ${num_cpus_all_node} / ${threads_per_core}`
+num_physical_cores_per_nodes=`expr ${num_cpus_per_node} / ${threads_per_core}`
+num_physical_cores_per_socket=`expr ${num_physical_cores_all_nodes} / ${num_socket}`
+num_physical_cores_per_numa=`expr ${num_physical_cores_all_nodes} / ${num_numa}`
+echo "Num physical cores per nodes: "$num_physical_cores_per_nodes
+echo "Num physical cores per socket: "$num_physical_cores_per_socket
+echo "Num physical cores per numa: "$num_physical_cores_per_numa
+
+th=`expr ${num_physical_cores_per_numa} / 2`  #${num_physical_cores_per_numa}  ##20
+if [ $th -le 10 ]
+then
+    th=${num_physical_cores_per_numa}
+fi
+
+while [ $num_physical_cores_per_nodes -gt $th ]
+do
+    num_physical_cores_per_nodes=`expr $num_physical_cores_per_nodes / 2`
+done
+
+num_physical_cores_per_rank=$num_physical_cores_per_nodes
+total_num_ranks=`expr ${num_physical_cores_all_nodes} / ${num_physical_cores_per_rank}`
+
+ranks_per_node=`expr ${total_num_ranks} / ${num_nodes}`
+echo "Cores per node: "$num_physical_cores_per_nodes
+echo "Total number of ranks: "${total_num_ranks}
+echo "Ranks per node: "${ranks_per_node}
+echo "#############################################"
+
+INDIR=$INPUT_DIR
+OUTDIR=$OUTPUT_DIR
+
+#* ranks: Number of mpi process that we want the pipeline to run on
+#* threads/shards: parameters to different tools in the pipeline, calculated as below
+ppn=${ranks_per_node}
+
+Sockets=$(cat compute_config | grep -E '^Socket\(s\)' | awk  '{print $2}')   #2
+Cores=$(cat compute_config | grep -E '^Core\(s\)' | awk  '{print $4}')  #56
+Thread=$(cat compute_config | grep -E '^Thread' | awk  '{print $4}')  #2
+
+a=$(( $(( ${Cores}*${Thread}*${Sockets} / $ppn )) - 2*${Thread} ))   #24 (Four threads are removed for IO)
+b=$(( $(( ${Cores}*${Sockets} )) / $ppn ))   #14
+
+if [ $a -lt 1 ]
+then
+    echo 'Number of cpus are less to run the pipeline.'
+    exit 0
+fi
+
+#N=$1
+#PPN=$2
+N=${total_num_ranks}
+PPN=${ranks_per_node}
+CPUS=$a
+THREADS=$a
+BINDING=socket
+
+echo "mode: "$mode
+
+## parameters
+READ1=${R1[@]}
+READ2=${R2[@]}
+READ3=${R3[@]}
+READI1=${I1[@]}
+
+echo "reads:"
+echo "READ1 $READ1"
+echo "READ2 $READ2"
+echo "READI1 $READI1"
+echo "R1PREFIX $R1PREFIX"
+echo "R3PREFIX $R3PREFIX"
+
+whitelist=""
+read_structure=""
+barcode_orientation=""
+bam_size=""
+sample_id=""
+output_format=""
+params=""
+outfile=""
+istart=""
+
+[[ -n $WHITELLIST ]] && whitelist="--whitelist $WHITELIST" && echo "Whitelist: $whitelist"
+[[ -n $READ_STRUCTURE ]] && read_structure="--read_structure $READ_STRUCTURE" && echo "Read Structure: $read_structure"
+[[ -n $BARCODE_ORIENTATION ]] && barcode_orientation="--barcode_orientation $BARCODE_ORIENTATION" && echo "Barcode Orientation: $barcode_orientation"
+[[ -n $SAMPLE_ID ]] && sample_id="--sample_id $SAMPLE_ID" && echo "sample_id: $sample_id"
+[[ -n $OUTPUT_FORMAT ]] && output_format="--output_format $OUTPUT_FORMAT" && echo "output_format: $output_format"
+[[ -n $PARAMS ]] && params="--params $PARAMS" && echo "params: $params"
+[[ -n $OUTFILE ]] && outfile="--outfile $OUTFILE" && echo "outfile: $outfile"
+if [ "$ISTART" == "True" ]
+then
+    istart="--istart"
+    echo "istart: $istart"
+fi
+
+echo "Input directory: $INDIR"
+echo "Output directory: $OUTDIR"
+mkdir -p ${OUTDIR}
+
+echo Starting run with $N ranks, $CPUS threads,$THREADS threads, $PPN ppn.
+# -in -sindex are required only once for indexing.
+# Todo : Make index creation parameterized.
+
+exec=dist_star.py
+mpiexec -bootstrap ssh -bind-to $BINDING -map-by $BINDING --hostfile hostfile -n $N -ppn $PPN python -u $exec --input $INDIR --output  $OUTDIR $TEMPDIR $REFDIR --index $REF --read1 $READ1 --read2 $READ2 --read3 $READ3 --cpus $CPUS --threads $THREADS --keep_unmapped ${whitelist} ${read_structure} ${barcode_orientation} ${bam_size} ${params} ${outfile} ${istart} ${sample_id} ${output_format} --r1prefix $R1PREFIX --r3prefix $R3PREFIX --mode $mode   2>&1 | tee ${OUTDIR}log.txt
+
+
+
+
